a1e2dd3645e8c5271671a2504d96c72c83a755cf
MichalPrzyl <michal.przyl@gmail.com>
2023-11-07 01:19:28 +0100

    ehh

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'myapp'
+    'django.contrib.staticfiles', 'blog'
+    
 ]
 
 MIDDLEWARE = [
--- backend/myapp/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Ludzik:
-    first_name = models.CharField(max_length=255, null=True, blank=True)
